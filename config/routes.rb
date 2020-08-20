5c5b9a9dbc98c1c4be4fc9bffbcce0ca8d316d0d
yann pfeiffer <pfeifferyann@gmail.com>
2020-08-20 20:17:05 +0200

    team

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   get '/bio', to: 'pages#bio', as: :bio
+  get '/team', to: 'pages#team', as: :team
 end
